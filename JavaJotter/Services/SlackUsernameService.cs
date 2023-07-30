aec4c4a9cb8de9bac9f85a68b7dcb632ee05fa4d
Lukas Olson <lukas@alumni.ubc.ca>
2023-07-30 16:43:50 -0700

    Updated how we're collecting usernames

--- JavaJotter/Services/SlackUsernameService.cs
+++ JavaJotter/Services/SlackUsernameService.cs
@@ -24,13 +24,41 @@ public class SlackUsernameService : IUsernameService
 
         _logger.Log($"Found {members.Count} users");
 
-        return members.Select(member
-            => new Username(member.Id, member.Name)).ToList();
+
+        return members.Select(member => Username(member.Id, member.Profile.FirstName, member.Profile.LastName))
+            .ToList();
     }
 
     public async Task<Username?> GetUsername(string id)
     {
         var user = await _slackClient.Users.Info(id);
-        return user is null ? null : new Username(user.Id, user.Name);
+        var firstName = user.Profile.FirstName;
+        var lastName = user.Profile.LastName;
+
+        return Username(user.Id, firstName, lastName);
     }
+
+    private static Username Username(string id, string firstName, string lastName, int maxLength = 7)
+    {
+        var lengthPerName = (maxLength - 1) / 2;
+
+        var shortFirstName = firstName.Length > lengthPerName ? firstName[..lengthPerName] : firstName;
+        var shortLastName = lastName.Length > lengthPerName ? lastName[..lengthPerName] : lastName;
+
+        var username = shortFirstName;
+
+        if (!string.IsNullOrEmpty(lastName))
+        {
+            username += $" {shortLastName}";
+        }
+        else
+        {
+            // When the last name is not available, take as many characters as allowed from the first name
+            username = firstName.Length > maxLength ? firstName[..maxLength] : firstName;
+        }
+
+        return new Username(id, username);
+    }
+
+
 }
